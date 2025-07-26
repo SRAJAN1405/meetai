d1fcb3afb2ff22b576b012f4d8e3f7e8fd7d7688
unknown <170554526+SRAJAN1405@users.noreply.github.com>
2025-07-26 13:31:34 +0530

    auth ui

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/app/modules/auth/ui/views/sign-up-view";
+const Page = () => {
+  return <SignUpView />;
+};
+export default Page;
+//localhost:3000/auth/sign-up
